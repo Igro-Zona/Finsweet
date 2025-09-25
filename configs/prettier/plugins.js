0b6cf7bfd869737d91a2222ae9f6dab960fa17fc
Igro-Zona <tim.bic22@gmail.com>
2025-09-26 00:10:06 +0300

    added prettier plugin for tailwind

--- configs/prettier/plugins.js
+++ configs/prettier/plugins.js
@@ -1,7 +1,8 @@
 // @ts-check
 
 const astroPlugin = "prettier-plugin-astro";
+const tailwindPlugin = "prettier-plugin-tailwindcss";
 
 /** @type {import("./types").Plugins} */
-const plugins = [astroPlugin];
+const plugins = [astroPlugin, tailwindPlugin];
 export default plugins;
